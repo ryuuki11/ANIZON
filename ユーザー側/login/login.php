ecd270e18085ebc67d457a8e4877b0d57338c0b2
ryuuki11 <2201370@s.asojuku.ac.jp>
2023-11-22 21:10:06 +0900

    ログイン、登録html,css編集完了

--- ユーザー側/login/login.php
+++ ユーザー側/login/login.php
@@ -3,16 +3,18 @@
     <head>
 
     <meta charset="UTF=8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/login.css">
     <title>login</title>
 </head>
 <body>
-    <form action="login-output.php" method="post">
-        <a><p>ログインID</a><br>
-        <input type="text" name="login"></p>
+<form action="login-output.php" method="post">
+        <p class="id">ログインID</p>
+        <p><input type="text" name="login"></p>
 
-        <a><p>パスワード</a><br>
-        <input type="password" name="password"></p><br>
+        <p>パスワード</p>
+        <p><input type="password" name="password"></p>
 
         <div><button type="submit" class="login">ログイン</button><br></div>
     </form>
